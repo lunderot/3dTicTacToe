c7b847c2c7da6bd333b50c29a67d01d640b2a971
Mattias Lunderot <matriz@live.se>
2014-07-27 21:52:45 +0200

    Chat now working, player handling added

--- server.js
+++ server.js
@@ -1,8 +1,72 @@
-var express = require("express");
-var app = express();
+var app = require("express")();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server, {log: true});
-	
+
+var combos = [
+	parseInt("111000000000000000000000000", 2),
+	parseInt("000111000000000000000000000", 2),
+	parseInt("000000111000000000000000000", 2),
+	parseInt("000000000111000000000000000", 2),
+	parseInt("000000000000111000000000000", 2),
+	parseInt("000000000000000111000000000", 2),
+	parseInt("000000000000000000111000000", 2),
+	parseInt("000000000000000000000111000", 2),
+	parseInt("000000000000000000000000111", 2)
+];
+
+function Player(socket, nickname)
+{
+	this.socket = socket;
+	this.nickname = nickname;
+	this.markers = 0;
+}
+
+Player.prototype.swapMarker = function(marker) {
+	this.markers ^= marker;
+};
+
+Player.prototype.resetMarkers = function() {
+	this.markers = 0;
+};
+
+Player.prototype.hasCombo = function(combo) {
+	return this.marker & combo == combo;
+};
+
+Player.prototype.getScore = function() {
+	var score = 0;
+	for (var i = combos.length - 1; i >= 0; i--) {
+		if(this.hasCombo(combos[i])) {
+			score++;
+		}
+			
+	};
+	return score;
+};
+
+function getPlayerIdFromSocket (socket) {
+	var i;
+	for (i = players.length - 1; i >= 0; i--) {
+		if(players[i].socket == socket)
+			break;
+	};
+	return i;
+}
+
+function getPlayerFromSocket (socket) {
+	var i;
+	for (i = players.length - 1; i >= 0; i--) {
+		if(players[i].socket == socket)
+			break;
+	};
+	return players[i];
+}
+
+//Player variables
+var players = [];
+
+
+//Setup the server
 server.listen(80);
 
 app.get("/", function(req, res){
@@ -11,4 +75,24 @@ app.get("/", function(req, res){
 
 app.get(/^(.+)$/, function(req, res){
 	res.sendfile("client/" + req.params[0]);
+});
+
+
+//Socket setup
+io.on("connection", function(socket) {
+	socket.on("disconnect", function() {
+		var index = getPlayerIdFromSocket(socket);
+		socket.broadcast.emit("chatmessage", players[index].nickname + " has disconnected.");
+		players.splice(index, 1);
+	});
+
+	socket.on("clientinfo", function(data) {
+		players.push(new Player(socket, data.nickname));
+		socket.broadcast.emit("chatmessage", data.nickname + " has connected.");
+	});
+
+	socket.on("chatmessage", function(data) {
+		var player = getPlayerFromSocket(socket);
+		io.sockets.emit("chatmessage", player.nickname + ": " + data);
+	});
 });
